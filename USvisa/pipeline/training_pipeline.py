d5c5855a4bd0f4e4cc2e0e7c9c515250b5648a28
RajatVerma01 <rajatverma11803@gmail.com>
2026-06-26 12:23:48 +0530

    Data Validation Completed

--- USvisa/pipeline/training_pipeline.py
+++ USvisa/pipeline/training_pipeline.py
@@ -2,15 +2,18 @@ import sys
 from USvisa.exception import USvisaException
 from USvisa.logger import logging
 from USvisa.components.data_ingestion import DataIngestion
+from USvisa.components.data_validation import DataValidation
 
-from USvisa.entity.config import (DataIngestionConfig)
+from USvisa.entity.config import (DataIngestionConfig,
+                                 DataValidationConfig)
                                          
-from USvisa.entity.artifact import (DataIngestionArtifact)
+from USvisa.entity.artifact import (DataIngestionArtifact,DataValidationArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
         
 
     
@@ -30,6 +33,32 @@ class TrainPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise USvisaException(e, sys) from e
+
+
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+        
         
         
         
@@ -40,6 +69,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             
         except Exception as e:
             raise USvisaException(e, sys)
--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="evidently")
+
 from dotenv import load_dotenv
 load_dotenv()  # loads MONGODB_CONNECTION_STRING (and any other vars) from .env
 
